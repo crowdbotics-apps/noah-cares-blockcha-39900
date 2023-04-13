9f6f371b7957f716add9d681c81afc3ffe6af187
Crowdbotics <team@crowdbotics.com>
2023-04-13 14:46:00 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/noah_cares_blockcha_39900/MainActivity.java
@@ -0,0 +1,15 @@
+package com.noah_cares_blockcha_39900;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "noah_cares_blockcha_39900";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "noah-cares-blockcha-39900",
+  url: "https://noah-cares-blockcha-39900.botics.co",
+  api: "https://noah-cares-blockcha-39900.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
